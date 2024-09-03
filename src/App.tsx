c2906752600e29f66f8d86a65b28fdcb00027546
Juneyoung Seo <juneyoungseo@Juneyoungui-MacBookPro.local>
2024-09-03 21:05:36 +0800

    #2 Developed Login Feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import TripPlanner from './pages/Main/TripPlanner';
 import Login from './pages/Auth/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Register from './pages/Auth/Register';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/TripPlanner" element={<TripPlanner />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Auth/Register.tsx
@@ -0,0 +1,7 @@
+const Register: React.FC = () => {
+    return (
+        <h2>hello</h2>
+    )
+}
+
+export default Register;
